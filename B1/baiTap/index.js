1cd2ab837387c4fe6f9b11dfeba3fee52eef5774
Petrichor101 <126537794+Petrichor101@users.noreply.github.com>
2023-12-05 20:02:27 +0700

    Add files via upload

---
+++ B1/baiTap/index.js
@@ -0,0 +1,19 @@
+showMessage1 = () => {
+    alert('Xin Chao')
+}
+
+showMessage2 = () => {
+    document.getElementById('messageDiv').innerHTML = 'Xin chào!';
+}
+
+clearMessage = () => {
+    document.getElementById('messageDiv').innerHTML = ''
+}
+
+showMessage3 = () => {
+    document.write('Xin chào')
+}
+
+showMessage4 = () => {
+    console.log("Xin chào")
+}
